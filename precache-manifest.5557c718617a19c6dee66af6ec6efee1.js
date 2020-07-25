747f4582c24fce0d73ae1c7380db23b35125f281
rochmat14 <abdulrochmat62@yahoo.com>
2020-07-25 20:00:55 +0700

    Updates

---
+++ precache-manifest.5557c718617a19c6dee66af6ec6efee1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a58da7c4b062d3a3f422ed28c10e372",
+    "url": "/rochmat14/react-host/index.html"
+  },
+  {
+    "revision": "b96aacafd9b8e85592e8",
+    "url": "/rochmat14/react-host/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7cc2d1c8443bdd7305fa",
+    "url": "/rochmat14/react-host/static/js/2.e66067ab.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/rochmat14/react-host/static/js/2.e66067ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96aacafd9b8e85592e8",
+    "url": "/rochmat14/react-host/static/js/main.20664e22.chunk.js"
+  },
+  {
+    "revision": "dbd3e21677a9f46c129f",
+    "url": "/rochmat14/react-host/static/js/runtime-main.ba27db06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rochmat14/react-host/static/media/logo.5d5d9eef.svg"
+  }
+]);
